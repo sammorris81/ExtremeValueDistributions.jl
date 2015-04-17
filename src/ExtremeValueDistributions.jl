49e6afa6c0969c622a62de80ecb10934ee04ff5d
Neal Grantham <ngranth@ncsu.edu>
2015-04-16 21:19:50 -0400

    load Distributions, Compat, & include generalizedpareto.jl

--- src/ExtremeValueDistributions.jl
+++ src/ExtremeValueDistributions.jl
@@ -1,5 +1,9 @@
 module ExtremeValueDistributions
 
-# package code goes here
+using Distributions
+import Distributions.@distr_support
+using Compat
+
+include("density/generalizedpareto.jl")
 
 end # module
