13783cf90c8db00ec44c285f431c859bfa67e2bd
sam-ubuntu <samorris@ncsu.edu>
2015-04-21 14:45:12 -0400

    Setting seed for examples

--- src/bayes/examples.jl
+++ src/bayes/examples.jl
@@ -8,6 +8,7 @@ using ExtremeValueDistributions
 #   log(σ) ~ Normal(0, 100)
 #        ξ ~ Normal(0, 1.0)
 # generate covariate data and simulated observations
+srand(1000)  # set seed
 n = 1000
 μₐ = 1.0
 σₐ = 2.0
@@ -18,6 +19,11 @@ z = reshape(rand(GeneralizedExtremeValue(μₐ, σₐ, ξₐ), n), n, 1)
 results = fit_mcmc(GeneralizedExtremeValue, z, iters=10000, burn=8000,
                    verbose=true, report=500)
 
+using Gadfly
+plot(x = 1:10000, y=results.βμpost, Geom.line)
+plot(x = 1:10000, y=exp(results.βσpost), Geom.line)
+plot(x = 1:10000, y=results.βξpost, Geom.line)
+
 # Simulated example - GEV Data
 #   Z ~ GeneralizedExtremeValue(μ, σ, ξ)
 #        μ = 1.0 + 2.0 * N(0, 1)
@@ -27,7 +33,7 @@ results = fit_mcmc(GeneralizedExtremeValue, z, iters=10000, burn=8000,
 #   βμ ~iid Normal(0, 100)
 #   βσ ~iid Normal(0, 50)
 #    ξ ~ Normal(0, 0.5)
-
+srand(1000)  # set seed
 n = 1000
 X = hcat(ones(n), rand(Normal(0, 1), n))
 βμₐ = [1.0, 2.0]
@@ -36,8 +42,8 @@ X = hcat(ones(n), rand(Normal(0, 1), n))
 σₐ  = exp(X * βσₐ)
 βξₐ = 0.1
 ξₐ  = 0.1
-z = reshape([rand(GeneralizedExtremeValue(μₐ[i], σₐ[i], ξₐ), 1)[1] for i = 1:n], n, 1)
-
+z = reshape([rand(GeneralizedExtremeValue(μₐ[i], σₐ[i], ξₐ), 1)[1] for i = 1:n],
+            n, 1)
 
 # to include covariates for μ, σ, or ξ, you need to include arguments
 # Xμ, Xσ, and Xξ
@@ -66,6 +72,7 @@ plot(x = 1:10000, y=results.βξpost, Geom.line)
 #   σ ~ InvGamma(0.1, 0.1)
 #   ξ ~ Normal(0, 0.5)
 # generate covariate data and simulated observations
+srand(1000)
 n = 1000
 μₐ = 1.0
 σₐ = 2.0
@@ -87,7 +94,7 @@ plot(x = 1:10000, y=results.βξpost, Geom.line)
 # priors:
 #   βσ ~iid Normal(0, 50)
 #    ξ ~ Normal(0, 0.5)
-
+srand(1000)
 n = 1000
 X = hcat(ones(n), rand(Normal(0, 1), n))
 βσₐ = [2.0, 1.3]
